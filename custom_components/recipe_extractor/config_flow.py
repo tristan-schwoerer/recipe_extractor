014a452e8e46e1c7e0bf310ffca9bca810c8fee0
Tristan Schwörer <tristans@mp.aau.dk>
2025-12-06 22:06:36 +0100

    Fix config flow

--- custom_components/recipe_extractor/config_flow.py
+++ custom_components/recipe_extractor/config_flow.py
@@ -129,44 +129,56 @@ class RecipeExtractorOptionsFlow(config_entries.OptionsFlow):
                 _LOGGER.info("Updating Recipe Extractor options")
                 return self.async_create_entry(title="", data=user_input)
 
+        # Get current options with proper defaults
+        current_api_key = self.config_entry.options.get(CONF_API_KEY, "")
+        current_todo_entity = self.config_entry.options.get(CONF_TODO_ENTITY)
+        current_model = self.config_entry.options.get(CONF_DEFAULT_MODEL, DEFAULT_MODEL)
+        current_convert = self.config_entry.options.get(CONF_CONVERT_UNITS, True)
+        
+        # Build schema with conditional defaults
+        schema_dict = {
+            vol.Optional(
+                CONF_API_KEY,
+                default=current_api_key,
+            ): selector.TextSelector(
+                selector.TextSelectorConfig(
+                    type=selector.TextSelectorType.PASSWORD,
+                ),
+            ),
+        }
+        
+        # Only add default for todo_entity if it has a value
+        if current_todo_entity:
+            schema_dict[vol.Optional(CONF_TODO_ENTITY, default=current_todo_entity)] = selector.EntitySelector(
+                selector.EntitySelectorConfig(
+                    domain="todo",
+                ),
+            )
+        else:
+            schema_dict[vol.Optional(CONF_TODO_ENTITY)] = selector.EntitySelector(
+                selector.EntitySelectorConfig(
+                    domain="todo",
+                ),
+            )
+        
+        schema_dict.update({
+            vol.Optional(
+                CONF_DEFAULT_MODEL,
+                default=current_model,
+            ): selector.SelectSelector(
+                selector.SelectSelectorConfig(
+                    options=AVAILABLE_MODELS,
+                    mode=selector.SelectSelectorMode.DROPDOWN,
+                ),
+            ),
+            vol.Optional(
+                CONF_CONVERT_UNITS,
+                default=current_convert,
+            ): selector.BooleanSelector(),
+        })
+        
         return self.async_show_form(
             step_id="init",
-            data_schema=vol.Schema(
-                {
-                    vol.Optional(
-                        CONF_API_KEY,
-                        default=self.config_entry.options.get(
-                            CONF_API_KEY, ""),
-                    ): selector.TextSelector(
-                        selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.PASSWORD,
-                        ),
-                    ),
-                    vol.Optional(
-                        CONF_TODO_ENTITY,
-                        default=self.config_entry.options.get(
-                            CONF_TODO_ENTITY, ""),
-                    ): selector.EntitySelector(
-                        selector.EntitySelectorConfig(
-                            domain="todo",
-                        ),
-                    ),
-                    vol.Optional(
-                        CONF_DEFAULT_MODEL,
-                        default=self.config_entry.options.get(
-                            CONF_DEFAULT_MODEL, DEFAULT_MODEL),
-                    ): selector.SelectSelector(
-                        selector.SelectSelectorConfig(
-                            options=AVAILABLE_MODELS,
-                            mode=selector.SelectSelectorMode.DROPDOWN,
-                        ),
-                    ),
-                    vol.Optional(
-                        CONF_CONVERT_UNITS,
-                        default=self.config_entry.options.get(
-                            CONF_CONVERT_UNITS, True),
-                    ): selector.BooleanSelector(),
-                }
-            ),
+            data_schema=vol.Schema(schema_dict),
             errors=errors,
         )
